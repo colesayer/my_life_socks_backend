7ddbe9f246bae741fa166e33a786316e58fd32d2
colesayer <colesayershapiro@gmail.com>
2018-02-06 16:02:56 -0500

    added sock model for saving

---
+++ app/models/sock.rb
@@ -0,0 +1,4 @@
+class Sock < ApplicationRecord
+  belongs_to :bump
+  belongs_to :design
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-resources :designs, only: [:index, :show, :create, :update, :destroy]
-resources :bumps, only: [:index, :show, :create, :update, :destroy]
+  resources :socks, only: [:index, :show, :create, :update, :destroy]
+  resources :designs, only: [:index, :show, :create, :update, :destroy]
+  resources :bumps, only: [:index, :show, :create, :update, :destroy]
 end
