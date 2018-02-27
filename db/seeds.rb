ab13f298f7f8b43e25f6da7a6c62212b3f69ab50
colesayer <colesayershapiro@gmail.com>
2018-02-27 18:58:38 -0500

    added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+design = Design.create(name: "base", design_url: "https://res.cloudinary.com/mylifesocks/image/upload/v1514072588/cream_ma9gqb.png", needle_count: 200, construction: "crew", date: 2018)
+bump = Bump.create(name: "base", bump_url: "https://res.cloudinary.com/mylifesocks/image/upload/v1514677121/wootv5uxbbwvwcc95ntu.jpg", needle_count: 200, construction: "crew", cushion: "full")
